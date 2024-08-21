fcfeb768609214afe5173f88b100cd8b9f4f97ed
xyzFahim <xyzfahim99@gmail.com>
2024-08-21 20:07:40 +0600

    Completed

--- backend/Routes/Api/task.js
+++ backend/Routes/Api/task.js
@@ -11,28 +11,55 @@ router.post("/", async (req, res) => {
 	};
 	const task = new Task(taskObj);
 	await task.save();
-	res.status(201).json(task);
+	res.status(201).send("Task created successfully");
 });
 
 //Get all tasks
 router.get("/", async (req, res) => {
-   try {
-     const tasks = await Task.find();
-     res.json(tasks);
-   } catch (error) {
-    res.status(500).send("Something Went Wrong");
-   }
-})
+	try {
+		const tasks = await Task.find();
+		res.json(tasks);
+	} catch (error) {
+		res.status(500).send("Something Went Wrong");
+	}
+});
 
 //Get Task by Id
-router.get ("/:id", async (req, res) => {
-    try {
-    const id = req.params.id;
-    const task = await Task.findById(id);
-    if (!task) return res.status(404).json("Task Not Found");
-    } catch (error) {
-      res.status(500).json({ message: "Something went wrong" });
-    }
+router.get("/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		const task = await Task.findById(id);
+		if (!task) return res.status(404).send("Task Not Found");
+		res.json(task);
+	} catch (error) {
+		res.status(500).json({ message: "Something went wrong" });
+	}
+});
+
+//Update Task by Id
+router.put("/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		const task = await Task.findByIdAndUpdate(id, req.body, {
+			new: true,
+		});
+		if (!task) return res.status(404).send("Task Not Found");
+		res.json(task);
+	} catch (error) {
+		res.status(500).send("Something Went Wrong");
+	}
+});
+
+//Delete Task by Id
+router.delete("/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		const task = await Task.findByIdAndDelete(id);
+		if (!task) return res.status(404).send("Task Not Found");
+		res.json("Task deleted" );
+	} catch (error) {
+		res.status(500).send("Something Went Wrong");
+	}
 });
 
 module.exports = router;
